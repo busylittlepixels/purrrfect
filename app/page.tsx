7ed7a802f7f64e6439570060f9958fedb043ef89
Ronan O'Leary <roleary81@gmail.com>
2025-01-30 17:27:38 +0100

    update to add test

--- app/page.tsx
+++ app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
         <Hero parallaxOffset={scrollY} />
         <div className="relative bg-background">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 space-y-24">
-	    <p>TESTING</p>
-            <BentoGrid />
+	          <BentoGrid />
+            <p>TESTING</p>
             <FeaturedArticles />
             <Categories />
           </div>
